2ac31c255fe8d04457957852be2a7be3393d6dc4
rubesh <rubesh.panda@coxandkings.com>
2017-10-26 09:49:26 +0530

    Schema change

---
+++ DistributionWebApi/DistributionWebApi/Controllers/ActivityController.cs
@@ -0,0 +1,310 @@
+﻿using DistributionWebApi.Models;
+using DistributionWebApi.Models.Activity;
+using DistributionWebApi.Mongo;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Newtonsoft.Json;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace DistributionWebApi.Controllers
+{
+    [RoutePrefix("Activity/Get")]
+    public class ActivityController : ApiController
+    {
+
+        protected static IMongoDatabase _database;
+        private readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+
+        [Route("ByCountries")]
+        [HttpPost]
+        [ResponseType(typeof(ActivityDefinition_SRP))]
+        public async Task<HttpResponseMessage> GetActivityByCountries(ActivitySearchByCountry_RQ param)
+        {
+            try
+            {
+                _database = MongoDBHandler.mDatabase();
+
+                //Get System Country Codes from Supplier Codes
+                var collection = _database.GetCollection<BsonDocument>("CountryMapping");
+                FilterDefinition<BsonDocument> filterCountry;
+                filterCountry = Builders<BsonDocument>.Filter.Empty;
+
+                filterCountry = filterCountry & Builders<BsonDocument>.Filter.AnyIn("SupplierCountryCode", param.CountryCodes.Distinct());
+                filterCountry = filterCountry & Builders<BsonDocument>.Filter.Regex("SupplierCode", new BsonRegularExpression(new Regex(param.RequestingSupplierCode, RegexOptions.IgnoreCase)));
+                ProjectionDefinition<BsonDocument> projectCountry = Builders<BsonDocument>.Projection.Include("CountryCode").Exclude("_id");
+
+                var searchCountryResult = await collection.Find(filterCountry).Project(projectCountry).ToListAsync();
+                var arrayOfStrings = searchCountryResult.Select(s => s["CountryCode"].AsString).ToArray();
+
+                //get Activities
+                IMongoCollection<BsonDocument> collectionActivity = _database.GetCollection<BsonDocument>("ActivityDefinitions");
+                FilterDefinition<BsonDocument> filter;
+                filter = Builders<BsonDocument>.Filter.Empty;
+                filter = filter & Builders<BsonDocument>.Filter.AnyIn("CountryCode", arrayOfStrings);
+
+                var TotalRecords = await collectionActivity.Find(filter).CountAsync();
+                var searchResult = await collectionActivity.Find(filter).Skip(param.PageSize * param.PageNo).Limit(param.PageSize).ToListAsync();
+
+                List<ActivityDefinition_PDP> searchedData = JsonConvert.DeserializeObject<List<ActivityDefinition_PDP>>(searchResult.ToJson());
+
+                ActivityDefinition_SRP resultList = new ActivityDefinition_SRP();
+
+                resultList.PageSize = param.PageSize;
+
+                int remainder = (int)TotalRecords % param.PageSize;
+                int quotient = (int)TotalRecords / param.PageSize;
+                if (remainder > 0)
+                    remainder = 1;
+
+                resultList.TotalPage = quotient + remainder;
+                resultList.CurrentPage = param.PageNo;
+                resultList.TotalNumberOfActivities = TotalRecords;
+                resultList.Activities = (from a in searchedData
+                                         select new Activities
+                                         {
+                                             TLGXActivityCode = a.TLGXActivityCode,
+                                             SupplierCompanyCode = a.SupplierCompanyCode,
+                                             SupplierProductCode = a.SupplierProductCode,
+                                             Category = a.Category,
+                                             Type = a.Type,
+                                             SubType = a.SubType,
+                                             Name = a.Name,
+                                             Description = a.Description,
+                                             Session = a.Session,
+                                             StartTime = a.StartTime,
+                                             EndTime = a.EndTime,
+                                             DaysOfTheWeek = a.DaysOfTheWeek,
+                                             PhysicalIntensity = a.PhysicalIntensity,
+                                             Overview = a.Overview,
+                                             Recommended = a.Recommended,
+                                             CountryName = a.CountryName,
+                                             CountryCode = a.CountryCode,
+                                             CityName = a.CityName,
+                                             CityCode = a.CityCode,
+                                             StarRating = a.StarRating,
+                                             NumberOfReviews = a.NumberOfReviews,
+                                             NumberOfLikes = a.NumberOfLikes,
+                                             NumberOfViews = a.NumberOfViews,
+                                             ActivityMedia = a.ActivityMedia,
+                                             Duration = a.Duration,
+                                         }).ToList();
+
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, resultList);
+                return response;
+
+            }
+            catch (Exception ex)
+            {
+                NLogHelper.Nlogger_LogError.LogError(ex, this.GetType().FullName, Request.GetActionDescriptor().ActionName, Request.RequestUri.PathAndQuery);
+                HttpResponseMessage response = Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Server Error. Contact Admin. Error Date : " + DateTime.Now.ToString());
+                return response;
+            }
+        }
+
+        [Route("ByCities")]
+        [HttpPost]
+        [ResponseType(typeof(ActivityDefinition_SRP))]
+        public async Task<HttpResponseMessage> GetActivityByCities(ActivitySearchByCity_RQ param)
+        {
+            try
+            {
+                _database = MongoDBHandler.mDatabase();
+
+                //Get System City Codes from Supplier Codes
+                var collection = _database.GetCollection<BsonDocument>("CityMapping");
+                FilterDefinition<BsonDocument> filterCountry;
+                filterCountry = Builders<BsonDocument>.Filter.Empty;
+
+                filterCountry = filterCountry & Builders<BsonDocument>.Filter.AnyIn("SupplierCityCode", param.CityCodes.Distinct());
+                filterCountry = filterCountry & Builders<BsonDocument>.Filter.Regex("SupplierCode", new BsonRegularExpression(new Regex(param.RequestingSupplierCode, RegexOptions.IgnoreCase)));
+                ProjectionDefinition<BsonDocument> projectCountry = Builders<BsonDocument>.Projection.Include("CityCode").Exclude("_id");
+
+                var searchCountryResult = await collection.Find(filterCountry).Project(projectCountry).ToListAsync();
+                var arrayOfStrings = searchCountryResult.Select(s => s["CityCode"].AsString).ToArray();
+
+                //get Activities
+                IMongoCollection<BsonDocument> collectionActivity = _database.GetCollection<BsonDocument>("ActivityDefinitions");
+                FilterDefinition<BsonDocument> filter;
+                filter = Builders<BsonDocument>.Filter.Empty;
+                filter = filter & Builders<BsonDocument>.Filter.AnyIn("CityCode", arrayOfStrings);
+
+                var TotalRecords = await collectionActivity.Find(filter).CountAsync();
+                var searchResult = await collectionActivity.Find(filter).Skip(param.PageSize * param.PageNo).Limit(param.PageSize).ToListAsync();
+
+                List<ActivityDefinition_PDP> searchedData = JsonConvert.DeserializeObject<List<ActivityDefinition_PDP>>(searchResult.ToJson());
+
+                ActivityDefinition_SRP resultList = new ActivityDefinition_SRP();
+
+                resultList.PageSize = param.PageSize;
+                int remainder = (int)TotalRecords % param.PageSize;
+                int quotient = (int)TotalRecords / param.PageSize;
+                if (remainder > 0)
+                    remainder = 1;
+
+                resultList.TotalPage = quotient + remainder;
+                resultList.CurrentPage = param.PageNo;
+                resultList.TotalNumberOfActivities = TotalRecords;
+                resultList.Activities = (from a in searchedData
+                                         select new Activities
+                                         {
+                                             TLGXActivityCode = a.TLGXActivityCode,
+                                             SupplierCompanyCode = a.SupplierCompanyCode,
+                                             SupplierProductCode = a.SupplierProductCode,
+                                             Category = a.Category,
+                                             Type = a.Type,
+                                             SubType = a.SubType,
+                                             Name = a.Name,
+                                             Description = a.Description,
+                                             Session = a.Session,
+                                             StartTime = a.StartTime,
+                                             EndTime = a.EndTime,
+                                             DaysOfTheWeek = a.DaysOfTheWeek,
+                                             PhysicalIntensity = a.PhysicalIntensity,
+                                             Overview = a.Overview,
+                                             Recommended = a.Recommended,
+                                             CountryName = a.CountryName,
+                                             CountryCode = a.CountryCode,
+                                             CityName = a.CityName,
+                                             CityCode = a.CityCode,
+                                             StarRating = a.StarRating,
+                                             NumberOfReviews = a.NumberOfReviews,
+                                             NumberOfLikes = a.NumberOfLikes,
+                                             NumberOfViews = a.NumberOfViews,
+                                             ActivityMedia = a.ActivityMedia,
+                                             Duration = a.Duration,
+                                         }).ToList();
+
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, resultList);
+                return response;
+
+            }
+            catch (Exception ex)
+            {
+                NLogHelper.Nlogger_LogError.LogError(ex, this.GetType().FullName, Request.GetActionDescriptor().ActionName, Request.RequestUri.PathAndQuery);
+                HttpResponseMessage response = Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Server Error. Contact Admin. Error Date : " + DateTime.Now.ToString());
+                return response;
+            }
+        }
+
+        [Route("ByTypes")]
+        [HttpPost]
+        [ResponseType(typeof(ActivityDefinition_SRP))]
+        public async Task<HttpResponseMessage> GetActivityByActivityTypes(ActivitySearchByTypes_RQ param)
+        {
+            try
+            {
+                _database = MongoDBHandler.mDatabase();
+
+                //get Activities
+                IMongoCollection<BsonDocument> collectionActivity = _database.GetCollection<BsonDocument>("ActivityDefinitions");
+                FilterDefinition<BsonDocument> filter;
+                filter = Builders<BsonDocument>.Filter.Empty;
+                filter = filter & Builders<BsonDocument>.Filter.AnyIn("Type", param.ActivityTypes);
+
+                var TotalRecords = await collectionActivity.Find(filter).CountAsync();
+                var searchResult = await collectionActivity.Find(filter).Skip(param.PageSize * param.PageNo).Limit(param.PageSize).ToListAsync();
+
+                List<ActivityDefinition_PDP> searchedData = JsonConvert.DeserializeObject<List<ActivityDefinition_PDP>>(searchResult.ToJson());
+
+                ActivityDefinition_SRP resultList = new ActivityDefinition_SRP();
+
+                resultList.PageSize = param.PageSize;
+                int remainder = (int)TotalRecords % param.PageSize;
+                int quotient = (int)TotalRecords / param.PageSize;
+                if (remainder > 0)
+                    remainder = 1;
+
+                resultList.TotalPage = quotient + remainder;
+                resultList.CurrentPage = param.PageNo;
+                resultList.TotalNumberOfActivities = TotalRecords;
+                resultList.Activities = (from a in searchedData
+                                         select new Activities
+                                         {
+                                             TLGXActivityCode = a.TLGXActivityCode,
+                                             SupplierCompanyCode = a.SupplierCompanyCode,
+                                             SupplierProductCode = a.SupplierProductCode,
+                                             Category = a.Category,
+                                             Type = a.Type,
+                                             SubType = a.SubType,
+                                             Name = a.Name,
+                                             Description = a.Description,
+                                             Session = a.Session,
+                                             StartTime = a.StartTime,
+                                             EndTime = a.EndTime,
+                                             DaysOfTheWeek = a.DaysOfTheWeek,
+                                             PhysicalIntensity = a.PhysicalIntensity,
+                                             Overview = a.Overview,
+                                             Recommended = a.Recommended,
+                                             CountryName = a.CountryName,
+                                             CountryCode = a.CountryCode,
+                                             CityName = a.CityName,
+                                             CityCode = a.CityCode,
+                                             StarRating = a.StarRating,
+                                             NumberOfReviews = a.NumberOfReviews,
+                                             NumberOfLikes = a.NumberOfLikes,
+                                             NumberOfViews = a.NumberOfViews,
+                                             ActivityMedia = a.ActivityMedia,
+                                             Duration = a.Duration,
+                                         }).ToList();
+
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, resultList);
+                return response;
+
+            }
+            catch (Exception ex)
+            {
+                NLogHelper.Nlogger_LogError.LogError(ex, this.GetType().FullName, Request.GetActionDescriptor().ActionName, Request.RequestUri.PathAndQuery);
+                HttpResponseMessage response = Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Server Error. Contact Admin. Error Date : " + DateTime.Now.ToString());
+                return response;
+            }
+        }
+
+        [Route("ByFacets")]
+        [HttpPost]
+        [ResponseType(typeof(void))]
+        public async Task<HttpResponseMessage> GetActivityByFacets(string[] param)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("Details/Code/{Code}")]
+        [HttpGet]
+        [ResponseType(typeof(ActivityDefinition_PDP))]
+        public async Task<HttpResponseMessage> GetActivityDetailsByCode(string Code)
+        {
+            try
+            {
+                _database = MongoDBHandler.mDatabase();
+
+                IMongoCollection<ActivityDefinition_PDP> collectionActivity = _database.GetCollection<ActivityDefinition_PDP>("ActivityDefinitions");
+
+                FilterDefinition<ActivityDefinition_PDP> filter;
+                filter = Builders<ActivityDefinition_PDP>.Filter.Empty;
+
+                filter = filter & Builders<ActivityDefinition_PDP>.Filter.Eq(x => x.Activity_Flavour_Id, Code);
+
+                var searchResult = collectionActivity.Find(filter).FirstOrDefaultAsync();
+
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, searchResult);
+                return response;
+
+            }
+            catch (Exception ex)
+            {
+                NLogHelper.Nlogger_LogError.LogError(ex, this.GetType().FullName, Request.GetActionDescriptor().ActionName, Request.RequestUri.PathAndQuery);
+                HttpResponseMessage response = Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Server Error. Contact Admin. Error Date : " + DateTime.Now.ToString());
+                return response;
+            }
+        }
+    }
+}
--- DistributionWebApi/DistributionWebApi/Models/ActivityDefinition.cs
+++ DistributionWebApi/DistributionWebApi/Models/ActivityDefinition.cs
@@ -1,11 +1,94 @@
-﻿using System;
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace DistributionWebApi.Models
+namespace DistributionWebApi.Models.Activity
 {
-    public class ActivityDefinition
+    public class ActivitySearchByCountry_RQ
+    {
+        public int PageSize { get; set; }
+        public int PageNo { get; set; }
+        public string RequestingSupplierCode { get; set; }
+        public string[] CountryCodes { get; set; }
+    }
+
+    public class ActivitySearchByCity_RQ
+    {
+        public int PageSize { get; set; }
+        public int PageNo { get; set; }
+        public string RequestingSupplierCode { get; set; }
+        public string[] CityCodes { get; set; }
+    }
+
+    public class ActivitySearchByTypes_RQ
+    {
+        public int PageSize { get; set; }
+        public int PageNo { get; set; }
+        public string RequestingSupplierCode { get; set; }
+        public string[] ActivityTypes { get; set; }
+    }
+
+    public class ActivityDefinition_PDP
+    {
+        [BsonId]
+        public string Activity_Flavour_Id { get; set; }
+        public string TLGXActivityCode { get; set; }
+        public string SupplierCompanyCode { get; set; }
+        public string SupplierProductCode { get; set; }
+        public string Category { get; set; }
+        public string Type { get; set; }
+        public string SubType { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Session { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+        public string DaysOfTheWeek { get; set; }
+        public string PhysicalIntensity { get; set; }
+        public string Overview { get; set; }
+        public string Recommended { get; set; }
+        public string CountryName { get; set; }
+        public string CountryCode { get; set; }
+        public string CityName { get; set; }
+        public string CityCode { get; set; }
+        public string StarRating { get; set; }
+        public string NumberOfPassengers { get; set; }
+        public string NumberOfReviews { get; set; }
+        public string NumberOfLikes { get; set; }
+        public string NumberOfViews { get; set; }
+        public string[] ActivityInterests { get; set; }
+        public List<Inclusions> Inclusions { get; set; }
+        public List<Exclusions> Exclusions { get; set; }
+        public string[] Highlights { get; set; }
+        public string[] TermsAndConditions { get; set; }
+        public List<ImportantInfoAndBookingPolicies> BookingPolicies { get; set; }
+        public List<Media> ActivityMedia { get; set; }
+        public ActivityDuration Duration { get; set; }
+        public List<ReviewScores> ReviewScores { get; set; }
+        public List<CustomerReviews> CustomerReviews { get; set; }
+        public ActivityLocation ActivityLocation { get; set; }
+        public List<TourGuideLanguages> TourGuideLanguages { get; set; }
+        public SupplierDetails SupplierDetails { get; set; }
+        public List<SimliarProducts> SimliarProducts { get; set; }
+        public List<ClassificationAttrributes> ClassificationAttrributes { get; set; }
+        public List<Deals> Deals { get; set; }
+        public List<Prices> Prices { get; set; }
+        public SystemMapping SystemMapping { get; set; }
+    }
+
+    public class ActivityDefinition_SRP
+    {
+        public long TotalNumberOfActivities { get; set; }
+        public int PageSize { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPage { get; set; }
+        public List<Activities> Activities { get; set; }
+
+    }
+
+    public class Activities
     {
         public string TLGXActivityCode { get; set; }
         public string SupplierCompanyCode { get; set; }
@@ -27,34 +110,20 @@ namespace DistributionWebApi.Models
         public string CityName { get; set; }
         public string CityCode { get; set; }
         public string StarRating { get; set; }
-        public int NumberOfPassengers { get; set; }
-        public int NumberOfReviews { get; set; }
-        public int NumberOfLikes { get; set; }
-        public int NumberOfViews { get; set; }
-        public string[] ActivityInterests { get; set; }
-        
-        public string[] Highlights { get; set; }
-        public string[] TermsAndConditions { get; set; }
-
-        public List<Inclusions> Inclusions { get; set; }
-        public List<Inclusions> Exclusions { get; set; }
-        public List<ImportantInfoAndBookingPolicies> BookingPolicies { get; set; }
+        public string NumberOfReviews { get; set; }
+        public string NumberOfLikes { get; set; }
+        public string NumberOfViews { get; set; }
         public List<Media> ActivityMedia { get; set; }
         public ActivityDuration Duration { get; set; }
-        public List<ReviewScores> ReviewScores { get; set; }
-        public List<CustomerReviews> CustomerReviews { get; set; }
-        public ActivityLocation ActivityLocation { get; set; }
-        public List<TourGuideLanguages> TourGuideLanguages { get; set; }
-        public SupplierDetails SupplierDetails { get; set; }
-        public List<SimliarProducts> SimliarProducts { get; set; }
-        public List<ClassificationAttrributes> ClassificationAttrributes { get; set; }
-        public List<Deals> Deals { get; set; }
-        public List<Prices> Prices { get; set; }
-        public SystemMapping SystemMapping { get; set; }
-        public List<SupplierAttributes> SupplierAttributes { get; set; }
+    }
+    
+    public class Inclusions
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
     }
 
-    public class Inclusions
+    public class Exclusions
     {
         public string Name { get; set; }
         public string Description { get; set; }
@@ -90,14 +159,14 @@ namespace DistributionWebApi.Models
     {
         public string Source { get; set; }
         public string Type { get; set; }
-        public double Score { get; set; }
+        public decimal? Score { get; set; }
     }
 
     public class CustomerReviews
     {
         public string Source { get; set; }
         public string Type { get; set; }
-        public double Score { get; set; }
+        public decimal? Score { get; set; }
         public string Author { get; set; }
         public string Date { get; set; }
         public string Comment { get; set; }
@@ -105,8 +174,8 @@ namespace DistributionWebApi.Models
 
     public class ActivityLocation
     {
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
+        public decimal? Latitude { get; set; }
+        public decimal? Longitude { get; set; }
         public string Address { get; set; }
         public string Location { get; set; }
         public string Area { get; set; }
@@ -145,9 +214,9 @@ namespace DistributionWebApi.Models
     {
         public string TLGXActivityCode { get; set; }
         public string DealText { get; set; }
-        public string[] Options { get; set; }
-        public string[] ActivityTypes { get; set; }
-        public List<Prices> TotalNetPrice { get; set; }
+        public string Options { get; set; }
+        public string ActivityType { get; set; }
+        public string TotalNetPrice { get; set; }
 
     }
 
@@ -160,7 +229,7 @@ namespace DistributionWebApi.Models
 
     public class Deals
     {
-        public decimal DealPrice { get; set; }
+        public decimal? DealPrice { get; set; }
         public string Currency { get; set; }
         public string DealText { get; set; }
         public string OfferTermsAndConditions { get; set; }
@@ -170,7 +239,7 @@ namespace DistributionWebApi.Models
     public class Prices
     {
         public string SupplierCurrency { get; set; }
-        public decimal NetPrice { get; set; }
+        public decimal? NetPrice { get; set; }
         public string PriceBasis { get; set; }
         public string PriceId { get; set; }
     }
@@ -179,13 +248,5 @@ namespace DistributionWebApi.Models
     {
         public string SystemName { get; set; }
         public string SystemID { get; set; }
-    }
-
-    public class SupplierAttributes
-    {
-        public string Group { get; set; }
-        public string Key { get; set; }
-        public string Value { get; set; }
-        public string Supplier { get; set; }
     }
 }
