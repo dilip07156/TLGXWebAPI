af10de557dd7da72b5e7cd144b4fc5e0a790c122
Rubesh Panda <rubesh.panda@coxandkings.com>
2018-11-06 12:20:08 +0530

    TMAP 787

--- DistributionWebApi/DistributionWebApi/Models/AccomodationMaster.cs
+++ DistributionWebApi/DistributionWebApi/Models/AccomodationMaster.cs
@@ -17,11 +17,16 @@ namespace DistributionWebApi.Models
         [Newtonsoft.Json.JsonProperty("_id")]
         public int CommonHotelId { get; set; }
         public string HotelName { get; set; }
-        public string Country { get; set; }
-        public string City { get; set; }
+        public string ProductCategory { get; set; }
+        public string ProductCategorySubType { get; set; }
+        public string CountryCode { get; set; }
+        public string CountryName { get; set; }
+        public string CityCode { get; set; }
+        public string CityName { get; set; }
+        public string StateCode { get; set; }
+        public string StateName { get; set; }
         public string StreetName { get; set; }
         public string StreetNumber { get; set; }
-        public string Street3 { get; set; }
         public string Street4 { get; set; }
         public string Street5 { get; set; }
         public string PostalCode { get; set; }
@@ -29,10 +34,15 @@ namespace DistributionWebApi.Models
         public string Location { get; set; }
         public string Area { get; set; }
         public string TLGXAccoId { get; set; }
-        public string ProductCategory { get; set; }
-        public string ProductCategorySubType { get; set; }
         public bool IsRoomMappingCompleted { get; set; }
-
+        public string Email { get; set; }
+        public string Fax { get; set; }
+        public string WebSiteURL { get; set; }
+        public string Telephone { get; set; }
+        public string FullAddress { get; set; }
+        public string HotelStarRating { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
     }
 }
 
