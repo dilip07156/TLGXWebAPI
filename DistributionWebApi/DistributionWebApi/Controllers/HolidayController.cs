3e77172fac400aaa577ce6bdb89d7f5b3e19197c
Vaibhav Jadhav <Vaibhav.Jadhav@coxandkings.com>
2019-02-05 16:27:40 +0530

    code changes completed for Search Functionality

--- DistributionWebApi/DistributionWebApi/Controllers/HolidayController.cs
+++ DistributionWebApi/DistributionWebApi/Controllers/HolidayController.cs
@@ -72,5 +72,149 @@ namespace DistributionWebApi.Controllers
                 return response;
             }
         }
+
+
+        [HttpPost]
+        [Route("System/GetHolidaySearch")]
+        [ResponseType(typeof(HolidayMappingSearchResult))]
+        public async Task<HttpResponseMessage> GetHolidaySearch(HolidaySearchRequestParams HolidaySearchRequestParams)
+        {
+
+            try
+            {
+                _database = MongoDBHandler.mDatabase();
+                HolidayMappingSearchResult resultList = new HolidayMappingSearchResult();
+                if (HolidaySearchRequestParams != null)
+                {
+                    IMongoCollection<DistributionWebApi.Models.HolidayModel> collectionHolidayMapping = _database.GetCollection<DistributionWebApi.Models.HolidayModel>("HolidayMapping");
+
+                    FilterDefinition<HolidayModel> filter;
+                    filter = Builders<HolidayModel>.Filter.Empty;
+
+                    if (!string.IsNullOrEmpty(HolidaySearchRequestParams.Country))
+                    {
+                        filter = filter & Builders<DistributionWebApi.Models.HolidayModel>.Filter.ElemMatch(x => x.Destinations, x => x.TLGXCountryCode.ToUpper() == HolidaySearchRequestParams.Country.ToUpper());
+                    }
+                    if (!string.IsNullOrEmpty(HolidaySearchRequestParams.CityCode))
+                    {
+                        filter = filter & Builders<DistributionWebApi.Models.HolidayModel>.Filter.ElemMatch(x => x.Destinations, x => x.TLGXCityCode.ToUpper() == HolidaySearchRequestParams.CityCode.ToUpper());
+                    }
+                    if (!string.IsNullOrEmpty(HolidaySearchRequestParams.HolidayName))
+                    {
+                        var escapedHolidayName = Regex.Escape(HolidaySearchRequestParams.HolidayName);
+                        filter = filter & Builders<DistributionWebApi.Models.HolidayModel>.Filter.Regex(x => x.ProductName, new BsonRegularExpression(new Regex(escapedHolidayName, RegexOptions.IgnoreCase)));
+                    }
+                    if (!string.IsNullOrEmpty(HolidaySearchRequestParams.HolidayFlavour))
+                    {
+                        var escapedHolidayFlavour = Regex.Escape(HolidaySearchRequestParams.HolidayFlavour);
+                        filter = filter & Builders<DistributionWebApi.Models.HolidayModel>.Filter.Regex(x => x.ProductFlavorName, new BsonRegularExpression(new Regex(escapedHolidayFlavour, RegexOptions.IgnoreCase)));
+                    }
+                    if (!string.IsNullOrEmpty(HolidaySearchRequestParams.Supplier))
+                    {
+                        var escapedSupplierName = Regex.Escape(HolidaySearchRequestParams.Supplier);
+                        filter = filter & Builders<DistributionWebApi.Models.HolidayModel>.Filter.Regex(x => x.SupplierName, new BsonRegularExpression(new Regex(escapedSupplierName, RegexOptions.IgnoreCase)));
+                        //filter = filter & Builders<DistributionWebApi.Models.HolidayModel>.Filter.Eq(x => x.SupplierName.ToLower(), HolidaySearchRequestParams.Supplier.ToLower());
+                    }
+                    if (!string.IsNullOrEmpty(HolidaySearchRequestParams.MappingStatus))
+                    {
+                        var escapedMappingStatus = Regex.Escape(HolidaySearchRequestParams.MappingStatus);
+                        filter = filter & Builders<DistributionWebApi.Models.HolidayModel>.Filter.Regex(x => x.UserReviewStatus, new BsonRegularExpression(new Regex(escapedMappingStatus, RegexOptions.IgnoreCase)));
+                    }
+
+                    if (HolidaySearchRequestParams.IsInterestsMissing)
+                    {
+                        filter = filter & Builders<DistributionWebApi.Models.HolidayModel>.Filter.Size(x => x.Interests, 0);
+                    }
+
+                    if (HolidaySearchRequestParams.IsTravellerTypeMissing)
+                    {
+                        filter = filter & Builders<DistributionWebApi.Models.HolidayModel>.Filter.Size(x => x.TravellerType, 0);
+                    }
+
+                    if (HolidaySearchRequestParams.IsComfortLevelMissing)
+                    {
+                        filter = filter & Builders<DistributionWebApi.Models.HolidayModel>.Filter.Size(x => x.ComfortLevel, 0);
+                    }
+
+                    if (HolidaySearchRequestParams.IsTravelFrequencyMissing)
+                    {
+                        filter = filter & Builders<DistributionWebApi.Models.HolidayModel>.Filter.Size(x => x.TravelFrequency, 0);                       
+                    }
+
+                    if (HolidaySearchRequestParams.IsStayTypeMissing)
+                    {
+                        filter = filter & Builders<DistributionWebApi.Models.HolidayModel>.Filter.Eq(x => x.StayType, string.Empty);
+                    }
+
+                    if (HolidaySearchRequestParams.IsTravelFrequencyMissing)
+                    {
+                        filter = filter & Builders<DistributionWebApi.Models.HolidayModel>.Filter.Size(x => x.TravelFrequency, 0);                    
+                    }
+                    if (HolidaySearchRequestParams.IsPaceOfHolidayMissing)
+                    {
+                        filter = filter & Builders<DistributionWebApi.Models.HolidayModel>.Filter.Eq(x => x.PaceOfHoliday, null);                    
+                    }
+
+                    if (HolidaySearchRequestParams.IsUSPMissing)
+                    {
+                        filter = filter & Builders<DistributionWebApi.Models.HolidayModel>.Filter.Eq(x => x.UniqueSellingPoints, null);
+                    }
+
+                    var TotalRecords = await collectionHolidayMapping.Find(filter).CountDocumentsAsync();
+
+                    List<HolidayModel> searchedData = new List<HolidayModel>();
+              
+                    if (TotalRecords != 0 && HolidaySearchRequestParams.PageSize != 0)
+                    {                     
+                        searchedData = await collectionHolidayMapping.Find(filter).Skip(HolidaySearchRequestParams.PageSize * HolidaySearchRequestParams.PageNo).Limit(HolidaySearchRequestParams.PageSize).ToListAsync();
+
+
+                        resultList.PageSize = HolidaySearchRequestParams.PageSize;
+                        resultList.CurrentPage = HolidaySearchRequestParams.PageNo;
+
+                        int remainder = (int)TotalRecords % HolidaySearchRequestParams.PageSize;
+                        int quotient = (int)TotalRecords / HolidaySearchRequestParams.PageSize;
+                        if (remainder > 0)
+                        {
+                            remainder = 1;
+                        }
+                        resultList.TotalPage = quotient + remainder;
+                    }
+                    else
+                    {
+                        resultList.TotalPage = 0;
+                    }
+
+                    resultList.CurrentPage = HolidaySearchRequestParams.PageNo;
+                    resultList.TotalNumberOfHolidays = TotalRecords;
+                    resultList.Holidays = (from a in searchedData
+                                           select new HolidaySearchResponse
+                                           {
+                                               NAKID = a.NakshatraHolidayId,
+                                               HolidayName = a.ProductName,
+                                               HolidayId = a.SupplierHolidayId,
+                                               Country = (a.Destinations.Count > 0 ? a.Destinations.First().TLGXCountryName : ""),
+                                               City = (a.Destinations.Count > 0 ? a.Destinations.First().TLGXCityName : ""),
+                                               Source = a.SupplierName,
+                                               FlavourId = a.ProductFlavorID,
+                                               HolidayFlavour = a.ProductFlavorName
+
+                                           }).ToList();
+
+
+                    return Request.CreateResponse(HttpStatusCode.OK, resultList);
+
+                }
+                else
+                    return null;
+
+            }
+            catch (Exception ex)
+            {
+                NLogHelper.Nlogger_LogError.LogError(ex, this.GetType().FullName, Request.GetActionDescriptor().ActionName, Request.RequestUri.PathAndQuery);
+                HttpResponseMessage response = Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Server Error. Contact Admin. Error Date : " + DateTime.Now.ToString());
+                return response;
+            }
+        }
     }
 }
